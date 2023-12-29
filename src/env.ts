d8ea485e4b7f2983b204ad77f691772cfc72c15b
RafaelDomingues-R2D2 <rafael.dominguesr2d2@gmai.com>
2023-12-29 12:47:13 -0300

    Fetch API

---
+++ src/env.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod'
+
+const envSchema = z.object({
+  NEXT_PUCLIC_API_BASE_URL: z.string().url(),
+})
+
+const parsedEnv = envSchema.safeParse(process.env)
+
+if (!parsedEnv.success) {
+  console.log(
+    'Invalid enviroment variables',
+    parsedEnv.error.flatten().fieldErrors,
+  )
+
+  throw new Error('Invalid environment variables.')
+}
+
+export const env = parsedEnv.data
